a0fc415ce69e2a672e006e250a611f71b3927c9c
Sergiu-19 <42917431+Sergiu-19@users.noreply.github.com>
2019-07-05 23:19:50 +0300

    github finder (api, async)

---
+++ ModernJS/GitHubFinder/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'c7f32ddfe0b97ad83134';
+    this.client_secret = 'a80870f30e8c1f1a59044c4fd328b5df691b46da';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
